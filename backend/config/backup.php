d68470e39b2e3226ac17d35f53d1223b8f1995f8
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-02-13 13:19:03 +0100

    Improved backup configuration

--- backend/config/backup.php
+++ backend/config/backup.php
@@ -119,7 +119,7 @@ return [
          * If not specified, the file extension will be .archive for MongoDB and .sql for all other databases
          * The file extension should be specified without a leading .
          */
-        'database_dump_file_extension' => '',
+        'database_dump_file_extension' => 'backup',
 
         'destination' => [
             /*
@@ -218,7 +218,7 @@ return [
         'notifiable' => Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => 'ocskon@gmail.com',
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
