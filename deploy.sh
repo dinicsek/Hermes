53c72e841168ff1cceadfe3e06bb5f8dde36d41e
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-02-16 12:52:31 +0100

    -------------

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,7 @@ set -e
 
 cd /data/hermes/
 
-previous_container_id=$(docker ps -aqf "name=hermes_container")
-if [ ! -z "$previous_container_id" ]; then
-  echo "Stopping previous container with ID: $previous_container_id"
-  docker stop "$previous_container_id"
-  docker rm "$previous_container_id"
-fi
+docker compose down
 
 cd ./backend/
 
@@ -34,8 +29,18 @@ echo "Cleaning up production image tarball..."
 rm "$latest_tarball"
 echo "Production image tarball cleaned up."
 
-echo "Starting production container..."
-container_name="hermes_container_$(date +"%Y-%m-%d_%H-%M-%S")"
 image_name=$(basename "$latest_tarball" .docker | cut -d ':' -f 1)
-docker run -d --name "$container_name" -p 80:80 -p 443:443 "$image_name"
+sed -i "s/REPLACE_IMAGE_NAME_HERE/$image_name/g" docker-compose.yml
+
+echo "Starting production container..."
+docker compose up -d
 echo "Production container started."
+
+#echo "Starting production container..."
+#container_name="hermes_container_$(date +"%Y-%m-%d_%H-%M-%S")"
+#docker run -d --name "$container_name" -p 80:80 -p 443:443 "$image_name"
+#echo "Production container started."
+
+echo "Deleteing old images..."
+docker image prune -a
+echo "Old images deleted."
