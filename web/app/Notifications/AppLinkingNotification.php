5807532486c51d2294691ec1553f0ed4eba27a58
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-02-20 14:28:43 +0100

    Updated the app linking notification

--- web/app/Notifications/AppLinkingNotification.php
+++ web/app/Notifications/AppLinkingNotification.php
@@ -27,12 +27,15 @@ class AppLinkingNotification extends Notification implements ShouldQueue
 
     public function toMail($notifiable): MailMessage
     {
+        $appDownloadUrl = route('mobile-app-download');
         return (new MailMessage)
             ->subject('Csapatod jóvá lett hagyva')
             ->greeting('Gratulálunk!')
             ->line(new HtmlString('A csapatod (' . e($this->teamName) . ') jelentkezése jóvá lett hagyva a <b>' . e($this->tournamentName) . '</b> versenyen való részvételhez.'))
-            ->line(new HtmlString('Amennyiben szeretnél értesítéseket kapni, mielőtt a te csapatod meccse következik, kérlek telepítsd az <a href="#">alkalmazásunkat</a>, és utána kattints az alábbi gombra.')) //TODO: add link
+            ->line(new HtmlString("Amennyiben szeretnél értesítéseket kapni, mielőtt a te csapatod meccse következik, kérlek telepítsd az <a href='{$appDownloadUrl}'>alkalmazásunkat</a>, és utána kattints az alábbi gombra.")) //TODO: add link
             ->action('Alkalmazás összekapcsolása', 'hermes://app-linking/' . $this->appLinkingToken)
+            ->line('Amennyiben nem tudod megnyitni a fentebbi gomb segítségével az alkalmazást, kérlek másold be a következő linket a böngésződ címsorába és válaszd a megnyitás alkalmazással opciót:')
+            ->line('hermes://app-linking/' . $this->appLinkingToken)
             ->line('Sok sikert kívánunk a versenyhez!');
     }
 
