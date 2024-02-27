221c6869f23ac4ffcc16597315625d3ee008c9af
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-02-27 09:41:01 +0100

    Made it possible to send notifications to teams

--- web/app/Filament/Manager/Resources/TeamResource/Pages/ViewTeam.php
+++ web/app/Filament/Manager/Resources/TeamResource/Pages/ViewTeam.php
@@ -4,7 +4,11 @@ namespace App\Filament\Manager\Resources\TeamResource\Pages;
 
 use App\Filament\Manager\Resources\TeamResource;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
+use Kreait\Firebase\Messaging\AndroidConfig;
+use Kreait\Firebase\Messaging\CloudMessage;
 
 class ViewTeam extends ViewRecord
 {
@@ -15,6 +19,28 @@ class ViewTeam extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('send_notification')->label('Értesítés küldése')->color('gray')->icon('heroicon-o-paper-airplane')->form([
+                TextInput::make('title')->label('Cím')->required(),
+                TextInput::make('body')->label('Tartalom')->required(),
+            ])->action(function (ViewTeam $livewire, array $data) {
+                $tokens = $livewire->getRecord()->push_tokens;
+
+                if ($tokens === []) {
+                    Notification::make()->title('Nem lett kiküldve értesítés, mivel nincsenek összekapcsolt eszközök.')->danger()->send();
+                    return;
+                }
+
+                $messaging = app('firebase.messaging');
+
+                $message = CloudMessage::new()->withNotification([
+                    'title' => $data['title'],
+                    'body' => $data['body'],
+                ])->withAndroidConfig(AndroidConfig::fromArray([
+                    'priority' => 'high',
+                ]));
+
+                $messaging->sendMulticast($message, $tokens);
+            }),
             Actions\DeleteAction::make(),
         ];
     }
