c573e236891a0039cdb6800645e4b495a768f366
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-02-27 14:02:57 +0100

    Made the notification sending more configurable

--- web/app/Filament/Manager/Resources/TeamResource/Pages/ViewTeam.php
+++ web/app/Filament/Manager/Resources/TeamResource/Pages/ViewTeam.php
@@ -4,7 +4,10 @@ namespace App\Filament\Manager\Resources\TeamResource\Pages;
 
 use App\Filament\Manager\Resources\TeamResource;
 use Filament\Actions;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 use Kreait\Firebase\Messaging\AndroidConfig;
@@ -20,9 +23,11 @@ class ViewTeam extends ViewRecord
     {
         return [
             Actions\Action::make('send_notification')->label('Értesítés küldése')->color('gray')->icon('heroicon-o-paper-airplane')->form([
-                TextInput::make('title')->label('Az értesítés címe')->required(),
-                TextInput::make('body')->label('Az értesítés tartalma')->required(),
-            ])->action(function (ViewTeam $livewire, array $data) {
+                TextInput::make('title')->helperText('Ez jelenik csak meg, amikor az értesítés le van csukva.')->label('Az értesítés címe')->required(),
+                Textarea::make('body')->label('Az értesítés tartalma')->required(),
+                Toggle::make('notify')->label('Rezgés és hangjelzés küldése')->default(true),
+                ColorPicker::make('color')->label('Értesítés színe')->default('#3b82f6'),
+            ])->modalSubmitAction(fn(Actions\StaticAction $action) => $action->label('Kiküldés'))->action(function (ViewTeam $livewire, array $data) {
                 $tokens = $livewire->getRecord()->push_tokens;
 
                 if ($tokens === []) {
@@ -32,17 +37,28 @@ class ViewTeam extends ViewRecord
 
                 $messaging = app('firebase.messaging');
 
-                $message = CloudMessage::new()->withNotification([
-                    'title' => $data['title'],
-                    'body' => $data['body'],
-                ])->withAndroidConfig(AndroidConfig::fromArray([
+                $androidConfig = AndroidConfig::fromArray([
                     'priority' => 'high',
                     'notification' => [
-                        'color' => '#3b82f6',
+                        'color' => $data['color'],
                         'sound' => 'default',
                         'vibrate_timings' => ['0.5s', '1s', '0.5s', '1s', '0.5s'],
                     ],
-                ]));
+                ]);
+
+                if ($data['notify'] === false) {
+                    $androidConfig = AndroidConfig::fromArray([
+                        'priority' => 'high',
+                        'notification' => [
+                            'color' => $data['color'],
+                        ],
+                    ]);
+                }
+
+                $message = CloudMessage::new()->withNotification([
+                    'title' => $data['title'],
+                    'body' => $data['body'],
+                ])->withAndroidConfig($androidConfig);
 
                 $messaging->sendMulticast($message, $tokens);
             }),
