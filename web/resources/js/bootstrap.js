d95863e664813e70f8295dfa4560afc2bc5c629a
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-02-28 12:43:22 +0100

    Again

--- web/resources/js/bootstrap.js
+++ web/resources/js/bootstrap.js
@@ -20,18 +20,31 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Pusher = Pusher;
 
-let echoConfig = {
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    enabledTransports: ['ws', 'wss'],
-};
-
 if (import.meta.env.VITE_PUSHER_PORT) {
-    echoConfig.wsPort = import.meta.env.VITE_PUSHER_PORT;
-    echoConfig.wssPort = import.meta.env.VITE_PUSHER_PORT;
-    echoConfig.forceTLS = (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https';
+    let echoConfig = {
+        broadcaster: 'pusher',
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+        wsPort: import.meta.env.VITE_PUSHER_PORT,
+        wssPort: import.meta.env.VITE_PUSHER_PORT,
+        forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https'
+    };
+
+    window.Echo = new Echo(echoConfig);
+} else {
+    let pusher = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+        wsHost: window.host,
+        httpHost: window.host,
+        wsPort: null,
+        httpPath: '/realtime',
+        wssPort: null,
+        wsPath: '/realtime',
+    })
+
+    window.Echo = new Echo({
+        broadcaster: 'pusher',
+        client: pusher,
+    });
 }
 
 window.Echo = new Echo(echoConfig);
