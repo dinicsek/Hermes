6ac10da2b48fc16f0a8acd91f0af2ddfe729dd14
GitButler <gitbutler@gitbutler.com>
2024-02-28 12:11:56 +0100

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - Update pusher config (refs/gitbutler/Update-pusher-config)
       branch head: d0eafe7dc05f5af227ac22db4e735b9583ea47b6
       - web/resources/js/bootstrap.js
     - master (refs/gitbutler/master)
       - web/resources/js/bootstrap.js
     - Maybe now
     (refs/gitbutler/Maybe-now-)
       branch head: 3207dcbdf7578db1c9ce0d99aa64e9d3d810dad2
    
    Your previous branch was: refs/heads/master
    
    The sha for that commit was: 6e22d32769db5870d3c03002ed22bb98c00ece46
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- web/resources/js/bootstrap.js
+++ web/resources/js/bootstrap.js
@@ -20,13 +20,18 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Pusher = Pusher;
 
-window.Echo = new Echo({
+let echoConfig = {
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
-});
+};
+
+if (import.meta.env.VITE_PUSHER_PORT) {
+    echoConfig.wsPort = import.meta.env.VITE_PUSHER_PORT;
+    echoConfig.wssPort = import.meta.env.VITE_PUSHER_PORT;
+}
+
+window.Echo = new Echo(echoConfig);
