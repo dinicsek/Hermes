3503c36da694af8366d4114221797d64b57c1eb5
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-02-29 09:21:22 +0100

    Made it possible to manually send app linking notifications to teams

--- web/app/Filament/Manager/Resources/TeamResource/Pages/ViewTeam.php
+++ web/app/Filament/Manager/Resources/TeamResource/Pages/ViewTeam.php
@@ -3,10 +3,14 @@
 namespace App\Filament\Manager\Resources\TeamResource\Pages;
 
 use App\Filament\Manager\Resources\TeamResource;
+use App\Helpers\AppLinking\AppLinkingHelper;
+use App\Notifications\AppLinkingNotification;
 use Filament\Actions;
 use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 use Kreait\Firebase\Messaging\AndroidConfig;
@@ -21,6 +25,31 @@ class ViewTeam extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('send_app_linking_notification')->label('Összekapcsolási értesítés küldése')->color('gray')->form([
+                Toggle::make('include_team_emails')->label('Csapat tagok értesítése')->default(true),
+                TagsInput::make('emails')
+                    ->label('E-mail címek')
+                    ->nestedRecursiveRules([
+                        'email'
+                    ]),
+            ])->action(function (ViewTeam $livewire, array $data) {
+                $team = $livewire->getRecord();
+
+                $appLinkingHelper = app(AppLinkingHelper::class);
+
+                if ($data['include_team_emails'])
+                    $appLinkingHelper->sendAppLinkingNotifications($team);
+
+                if ($data['emails'] !== null) {
+                    foreach ($data['emails'] as $email) {
+                        $token = $appLinkingHelper->generateAppLinkingToken($team->id, $email);
+                        \Illuminate\Support\Facades\Notification::route('mail', $email)
+                            ->notify(new AppLinkingNotification($team->name, $team->tournament->name, $token));
+                    }
+                }
+
+                Notification::make()->title('Az értesítések sikeresen kiküldve.')->success()->send();
+            }),
             Actions\Action::make('send_notification')->label('Értesítés küldése')->color('gray')->icon('heroicon-o-paper-airplane')->form([
                 TextInput::make('title')->helperText('Ez jelenik csak meg, amikor az értesítés le van csukva.')->label('Az értesítés címe')->required(),
                 Textarea::make('body')->label('Az értesítés tartalma')->required(),
