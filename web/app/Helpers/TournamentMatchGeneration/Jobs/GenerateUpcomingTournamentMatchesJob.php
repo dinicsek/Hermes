1a219b24810bbe670c00b864f07abdf363795fda
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-03-19 18:34:32 +0100

    Fixed the elimination generator running on non elimination matches too

--- web/app/Helpers/TournamentMatchGeneration/Jobs/GenerateUpcomingTournamentMatchesJob.php
+++ web/app/Helpers/TournamentMatchGeneration/Jobs/GenerateUpcomingTournamentMatchesJob.php
@@ -4,6 +4,8 @@ namespace App\Helpers\TournamentMatchGeneration\Jobs;
 
 use App\Data\TournamentMatchData;
 use App\Helpers\TournamentMatchGeneration\EliminationTournamentMatchGenerator;
+use App\Models\Data\RoundConfiguration;
+use App\Models\Enums\RoundMode;
 use App\Models\Enums\TournamentMatchWinner;
 use App\Models\Team;
 use App\Models\Tournament;
@@ -25,16 +27,18 @@ class GenerateUpcomingTournamentMatchesJob implements ShouldQueue
     {
         $tournament = Tournament::find($this->previousMatchData->tournament_id);
 
-        $generator = new EliminationTournamentMatchGenerator();
+        if (RoundConfiguration::from(collect($tournament->round_settings)->firstWhere('round', '=', $this->previousMatchData->round))->mode === RoundMode::ELIMINATION) {
+            $generator = new EliminationTournamentMatchGenerator();
 
-        if ($this->previousMatchData->is_final || $this->previousMatchData->winner === null) {
-            return;
+            if ($this->previousMatchData->is_final || $this->previousMatchData->winner === null) {
+                return;
+            }
+
+            $winnerId = $this->previousMatchData->winner === TournamentMatchWinner::HOME_TEAM ? $this->previousMatchData->home_team_id : $this->previousMatchData->away_team_id;
+            $loserId = $this->previousMatchData->winner === TournamentMatchWinner::HOME_TEAM ? $this->previousMatchData->away_team_id : $this->previousMatchData->home_team_id;
+
+            $generator->generate($tournament, Team::find($winnerId), $this->previousMatchData->round, $this->previousMatchData->elimination_round, $this->previousMatchData->elimination_level);
+            $generator->generate($tournament, Team::find($loserId), $this->previousMatchData->round, $this->previousMatchData->elimination_round, $this->previousMatchData->elimination_level + 1);
         }
-
-        $winnerId = $this->previousMatchData->winner === TournamentMatchWinner::HOME_TEAM ? $this->previousMatchData->home_team_id : $this->previousMatchData->away_team_id;
-        $loserId = $this->previousMatchData->winner === TournamentMatchWinner::HOME_TEAM ? $this->previousMatchData->away_team_id : $this->previousMatchData->home_team_id;
-
-        $generator->generate($tournament, Team::find($winnerId), $this->previousMatchData->round, $this->previousMatchData->elimination_round, $this->previousMatchData->elimination_level);
-        $generator->generate($tournament, Team::find($loserId), $this->previousMatchData->round, $this->previousMatchData->elimination_round, $this->previousMatchData->elimination_level + 1);
     }
 }
